27089234cd8616f43ea11d7fe758131a50afc295
Azghan Ahmad <124713294+AzghanAhmad@users.noreply.github.com>
2026-01-23 23:14:10 +0500

    Backend folder updated

--- backend/seed.sql
+++
@@ -1,6 +0,0 @@
-USE word_tracker;
-INSERT IGNORE INTO `users` (`id`, `username`, `email`, `password_hash`, `created_at`, `updated_at`) VALUES
-(1, 'dsfe@gmail', 'dsfe@gmail.com', '$2y$10$awAdBPks3Kc35BdIFSVYNeCsyRpqUmOk.umha.YMjavxukkufz3wq', '2026-12-05 17:18:42', '2026-12-05 17:18:42'),
-(2, 'dsfe@gm4ail', 'bn@gmail.com', '$2y$10$Re6bbAJBsenclfWJI2bgYu1G6UpexxrVXH.enbeQd2ZIWYRUBySRO', '2026-12-05 17:20:24', '2026-12-05 17:20:24'),
-(3, 'ygygyg', 'ytftf@gmail.com', '$2y$10$Cvj9hqaB1q0HGqly3kbMmuS7EFVtJv8W/G3SJLL4zyM40W8BhftM6', '2026-12-06 15:40:20', '2026-12-06 15:40:20'),
-(4, 'jshd', 'saldj@gmail.com', '$2y$10$RobcF67l.NdDazhF5K8nK.El5FbUWo.hKuqB9x/q9cQNScELCKdPS', '2026-12-07 17:28:23', '2026-12-07 17:28:23');
